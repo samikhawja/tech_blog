7f130debc66baac2607b8d6f1258feaced4fb859
samikhawja <skhawja11@gmail.com>
2021-10-30 15:43:06 -0700

    finshed postJS in model

--- models/Comment.js
+++ models/Comment.js
@@ -19,6 +19,7 @@ Comment.init(
     },
     post_id: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         references: {
             model: 'post',
             key: 'id'
@@ -26,6 +27,7 @@ Comment.init(
     },
     user_id: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         references: {
             model: 'user',
             key: 'id'
