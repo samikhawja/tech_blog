b157d5b6f506f70fe0c65449651f2f23367ac96f
samikhawja <skhawja11@gmail.com>
2021-10-30 17:05:01 -0700

    indexJS in seeds complete

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const seedComment = require('./comment');
+const seedPost = require('./post');
+const seedUser = require('./user');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  await seedComment();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  await seedPost();
+  console.log('\n----- POSTS SEEDED -----\n');
+
+  await seedUser();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
